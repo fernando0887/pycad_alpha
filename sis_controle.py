3d31480c518de01d63e1f84b77f90e9e07d26f5d
fernando0887 <50933935+fernando0887@users.noreply.github.com>
2021-06-29 17:36:54 -0300

    Add files via upload

--- sis_controle.py
+++ sis_controle.py
@@ -1,150 +1,191 @@
 import os
-import pandas as pd
+#import pandas as pd
+from PySimpleGUI import PySimpleGUI as sg
 
 
 
 def cad_fis(cad_fis):
-        cad_fis = {'Nome: ' + input('Nome: ') + ' ' + 'Sobrenome: ' + input('Sobrenome: ') + ', ' +
-                   'Endereço: ' + input('Endereço: ') +  ', ' + 'Número: ' + input('Número: ') + ', ' +'Bairro: ' + input('Bairro: ') + ','
-                   + 'Cidade: ' + input('Cidade: ') + ', ' + 'Estado: ' + input('Estado: ') + ', ' + 'Telefone: ' + input('Telefone: ')
-                   + ', ' + 'Email: ' + input('Email: ') + ', ' + 'CPF: ' + input('CPF: ') }
-        arquivo = open('cadastrofisico.csv', 'a')
-        arquivo.writelines(cad_fis)
-        arquivo.writelines('\n')
-        arquivo.close()
-        return print('Dados cadastrados: ', cad_fis)
+    sg.theme('Reddit')
+    layout = [
+                  [sg.Text('Nome: ') , sg.Input('')],
+                  [sg.Text('Sobrenome: '), sg.Input('')],
+                  [sg.Text('Endereço: '),  sg.Input('')],
+                  [sg.Text('Número: '), sg.Input('')],
+                  [sg.Text('Bairro: '), sg.Input('')],
+                  [sg.Text('Cidade: '), sg.Input('')],
+                  [sg.Text('Estado: '), sg.Input('')],
+                  [sg.Text('Telefone: '), sg.Input('')],
+                  [sg.Text('Email: '), sg.Input('')],
+                  [sg.Text('CPF: '),  sg.Input('')],
+                  [sg.Button('Finalizar cadastro'), sg.Button('Voltar')]
+    ]
+    #arquivo = open('cadastrofisico.csv', 'a')
+    #arquivo.writelines(cad_fis)
+    #arquivo.writelines('\n')
+    #arquivo.close()
+
+    return sg.Window('Cadastro pessoa física', layout=layout, finalize=True)
 
 
 
 def cad_jur(cad_jur):
-        cad_jur = {'Nome: ' + input('Nome: ') + ' ' + 'Sobrenome: ' + input('Sobrenome: ') + ', ' +
-                   'Endereço: ' + input('Endereço: ') +  ', ' + 'Número: ' + input('Número: ') + ', ' +'Bairro: ' + input('Bairro: ') + ','
-                   + 'Cidade: ' + input('Cidade: ') + ', ' + 'Estado: ' + input('Estado: ') + ', ' + 'Telefone: ' + input('Telefone: ')
-                   + ', ' + 'Email: ' + input('Email: ') + ', ' + 'CNPJ':  + input('CNPJ: ') }
-        arquivo = open('cadastrojuridico.csv', 'a')
-        arquivo.writelines(cad_jur)
-        arquivo.writelines('\n')
-        arquivo.close()
-        return print('Dados da empresa cadastrada: ', cad_jur)
+    sg.theme('Reddit')
+    layout = [
+        [sg.Text('Nome da Empresa: '), sg.Input('')],
+        [sg.Text('Endereço: '), sg.Input('')],
+        [sg.Text('Número: '), sg.Input('')],
+        [sg.Text('Bairro: '), sg.Input('')],
+        [sg.Text('Cidade: '), sg.Input('')],
+        [sg.Text('Estado: '), sg.Input('')],
+        [sg.Text('Telefone: '), sg.Input('')],
+        [sg.Text('Email: '), sg.Input('')],
+        [sg.Text('CNPJ: '), sg.Input('')],
+        [sg.Button('Finalizar cadastro'), sg.Button('Voltar')]
 
+    ]
+        #arquivo = open('cadastrojuridico.csv', 'a')
+        #arquivo.writelines(cad_jur)
+        #arquivo.writelines('\n')
+        #arquivo.close()
+    return sg.Window('Cadastro jurídico ', layout= layout, finalize= True)
 
 
 def cad_prod(cad_prod):
-        cad_prod = {'Nome: ' + input('Nome: ') + ' ' + 'Código: ' + input('Código do produto: ') + ', ' +
-                   'Quantidade: ' + input('Quantidade: ') +  ', ' + 'Preço: ' + input('Preço: R$  ') }
-        arquivo = open('cadastroproduto.csv', 'a')
-        arquivo.writelines(cad_prod)
-        arquivo.writelines('\n')
-        arquivo.close()
-        return print('Produto: ', cad_prod)
+    sg.theme('Reddit')
+    layout = [
+        [sg.Text('Nome do produto: '), sg.Input('')],
+        [sg.Text('Código: '), sg.Input('')],
+        [sg.Text('Quantidade: '), sg.Input('')],
+        [sg.Text('Preço:'), sg.Input('')],
+        [sg.Button('Finalizar cadastro'), sg.Button('Voltar')]
+    ]
+        #arquivo = open('cadastroproduto.csv', 'a')
+        #arquivo.writelines(cad_prod)
+        #arquivo.writelines('\n')
+        #arquivo.close()
+    return sg.Window('Cadastro de produtos', layout=layout, finalize=True)
 
 
 
 def pesqfis(pesqfis):
-    nome = input('Insira o nome:')
-    with open('cadastrofisico.csv', 'r') as arquivo:
-        for f in arquivo.readlines():
-            if(f.find(nome) > -1):
-                print(f)
-            else:
-                print('Nome não encontrado.')
+    sg.theme('Reddit')
+    layout = [
+        [sg.Text('Nome a buscar:'), sg.Input('')],
+        [sg.Button('Buscar'), sg.Button('Voltar')]
+    ]
 
+    #with open('cadastrofisico.csv', 'r') as arquivo:
+     #   for f in arquivo.readlines():
+      #      if(f.find(nome) > 1):
+       #         print(f)
+        #    else:
+         #       print('Nome não encontrado.')
+    return sg.Window('Busca pessoa física', layout=layout, finalize=True)
 
 
 def pesqjur(pesqjur):
-    nome = input('Insira o nome:')
-    with open('cadastrojuridico.csv', 'r') as arquivo:
-        for f in arquivo.readlines():
-            if (f.find(nome) > 1):
-                print(f)
-            else:
-                print('Nome não encontrado.')
+    sg.theme('Reddit')
+    layout = [
+        [sg.Text('Nome a buscar:'), sg.Input('')],
+        [sg.Button('Buscar'), sg.Button('Voltar')]
+    ]
+   # with open('cadastrojuridico.csv', 'r') as arquivo:
+    #    for f in arquivo.readlines():
+     #       if (f.find(nome) > 1):
+      #          print(f)
+       #     else:
+        #        print('Nome não encontrado.')
+    return sg.Window('Busca pessoa jurídica', layout=layout, finalize=True)
 
 
 
 def pesqprod(pesqprod):
-    nome = input('Insira o nome:')
-    with open('cadastroproduto.csv', 'r') as arquivo:
-        for f in arquivo.readlines():
-            if (f.find(nome) > -1):
-                print(f)
-            else:
-                print('Produto não encontrado.')
+    sg.theme('Reddit')
+    layout = [
+        [sg.Text('Nome do produto:'), sg.Input('')],
+        [sg.Button('Buscar'), sg.Button('Voltar')]
+    ]
+    #with open('cadastroproduto.csv', 'r') as arquivo:
+     #   for f in arquivo.readlines():
+      #      if (f.find(nome) > -1):
+       #         print(f)
+        #    else:
+         #       f.find(nome) == 0
+          #      print('Produto não encontrado.')
+    return sg.Window('Busca produto', layout=layout, finalize=True)
 
 
+def tela_inicial():
+    layout = [
 
 
+        [sg.Text('1- Cadastro de pessoa física'), sg.Button('Selecionar 1')],
+        [sg.Text('2- Cadastro de pessoa jurídica'), sg.Button('Selecionar 2')],
+        [sg.Text('3- Cadastro de produto'), sg.Button('Selecionar 3')],
+        [sg.Text('4- Busca pessoa física'), sg.Button('Selecionar 4')],
+        [sg.Text('5- Busca pessoa jurídica'), sg.Button('Selecionar 5')],
+        [sg.Text('6- Busca produto'), sg.Button('Selecionar 6')],
+   ]
+    return sg.Window('PyCads alpha', layout=layout, finalize=True)
+
+janela1, janela2, janela3, janela4, janela5, janela6, janela7 = tela_inicial(), None, None, None, None, None, None
 
 while True:
+    window,event,values = sg.read_all_windows()
 
 
-        print(' ***** PyCads alpha *****\n\n '
-              
-                        'Selecione a opção desejada:\n'
-                        '1- Cadastro de pessoa física\n'
-                        '2- Cadastro de pessoa jurídica\n'
-                        '3- Cadastro de produto\n'
-                        '4- Busca pessoa física\n'
-                        '5- Busca pessoa jurídica\n'
-                        '6- Busca produto\n'
-                        '0- Sair\n')
+    if window == janela1 and event == sg.WINDOW_CLOSED:
+        break
+
+    if window == janela1 and event == 'Selecionar 1':
+        janela2 == cad_fis(cad_fis)
+        janela1.hide()
+
+    if window == janela2 and event == 'Voltar':
+        janela2.hide(cad_fis)
+        janela1.un_hide()
 
 
+    if window == janela1 and event == 'Selecionar 2':
+        janela3 == cad_jur(cad_jur)
+        janela1.hide()
 
-        menu = input('Opção:')
-        if menu == '1':
+    if window == janela3 and event == 'Voltar':
+        janela3.hide()
+        janela1.un_hide()
 
-                cad_fis(cad_fis)
-                s = input('Voltar ao menu principal? (digite S ou N para sair):')
-                if s == 'S' or 's':
-                        continue
-                else:
-                        s == 'N' or 'n'
-                        break
+    if window == janela1 and event == 'Selecionar 3':
+      janela4 == cad_prod(cad_prod)
+      janela1.hide()
 
+    if window == janela4 and event == 'Voltar':
+        janela4.hide()
+        janela1.un_hide()
 
+    if window == janela1 and event == 'Selecionar 4':
+        janela5 == pesqfis(pesqfis)
+        janela1.hide()
 
-        elif menu == '2':
-                cad_jur(cad_jur)
-                s = input('Voltar ao menu principal? (digite S ou N para sair):')
-                if s == 'S' or 's':
-                       continue
-                else:
-                       break
+    if window == janela5 and event == 'Voltar':
+        janela5.hide()
+        janela1.un_hide()
 
+    if window == janela1 and event == "Selecionar 5":
+        janela6 == pesqjur(pesqjur)
+        janela1.hide()
 
+    if window == janela6 and event == 'Voltar':
+        janela6.hide()
+        janela1.un_hide()
 
-        elif menu == '3':
-                cad_prod(cad_prod)
-                s = input('Voltar ao menu principal? (digite S ou N para sair):')
-                if s == 'S' or 's':
-                      continue
-                else:
-                       break
+    if window == janela1 and event == 'Selecionar 6':
+        janela7 ==  pesqprod(pesqprod)
+        janela1.hide()
 
+    if window == janela7 and event == 'Voltar':
+        janela7.hide()
+        janela1.un_hide()
 
-
-        elif menu == '4':
-                pesqfis(pesqfis)
-                break
-
-
-
-        elif menu == '5':
-                pesqjur(pesqjur)
-                break
-
-
-
-        elif menu == '6':
-                pesqprod(pesqprod)
-                break
-
-
-
-        elif menu == '0':
-                print('*****Saindo*****')
-                break
 
 
 
